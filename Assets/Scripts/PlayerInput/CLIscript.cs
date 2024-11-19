2123190e997d68c112dca479ff8a44468125ae14
IndustrialBro <124711765+IndustrialBro@users.noreply.github.com>
2024-11-19 17:11:10 +0100

    Changed CliScript.cs so the regex makes a little more sense
    I will never surrender the regex! Even if there are better ways of doing it there will always be the regex! Look upon my regex, ye mighty, and despair!

--- Assets/Scripts/PlayerInput/CLIscript.cs
+++ Assets/Scripts/PlayerInput/CLIscript.cs
@@ -16,7 +16,7 @@ public sealed class CLIscript : MonoBehaviour
     [SerializeField]
     TMP_InputField iField;
 
-    Regex r = new Regex(" ");
+    Regex r = new Regex("u[1-9]");
 
     private void Awake()
     {
@@ -32,7 +32,7 @@ public sealed class CLIscript : MonoBehaviour
         
         if (Input.GetButtonDown("Submit"))
         {
-            string noWhiteSpace = r.Replace(iField.text, "");
+            string noWhiteSpace = iField.text.Trim();
             
             if(noWhiteSpace != string.Empty)
                 SendCommand(iField.text);
@@ -47,14 +47,40 @@ public sealed class CLIscript : MonoBehaviour
         ShowOutput(input);
 
         string[] splitInput = input.Split(' ');
-        string[] units = splitInput[0].Split(',');
+        
+        for(int i = 0; i < splitInput.Length; i++) splitInput[i] = splitInput[i].ToLower();
+
+        List<string> units = new List<string>();
+        if (splitInput[0] == "all")
+        { //Všechny jednotky budou trsat
+            CommandInterpreter.Instance.InterpretCommand(splitInput[1], GetArgs(splitInput, 2), ComListThingamabob.Instance.GetListeners(new string[]{ "all" } ));
+        }
+        else if (r.IsMatch(splitInput[0]))
+        { //Jen specifikované jednotky budou trsat
+            int i;
+            for (i = 0; i < splitInput.Length; i++)
+            {
+                if (r.IsMatch(splitInput[i])) units.Add(splitInput[i]);
+                else break;
+            }
+            Debug.Log($"Jednotky: {units.Count}");
+
+            CommandInterpreter.Instance.InterpretCommand(splitInput[i], GetArgs(splitInput, i + 1), ComListThingamabob.Instance.GetListeners(units.ToArray()));
+        } 
+        else
+        { //Pøíkaz není o trsání
+
+        }
+    }
+    string[] GetArgs(string[] a, int startIndex)
+    {
         List<string> args = new List<string>();
-        for(int i = 2; i < splitInput.Length; i++)
+        for(int i = startIndex; i < a.Length; i++)
         {
-            args.Add(splitInput[i].ToLower());
+            args.Add(a[i]);
         }
 
-        CommandInterpreter.Instance.InterpretCommand(splitInput[1].ToLower(), args.ToArray(), ComListThingamabob.Instance.GetListeners(units));
+        return args.ToArray();
     }
     public void ShowOutput(string o)
     {
